db089f968bbd0ae3e8a21c95b86760b3ddaa4bcd
Michael Webb <webbmichael@hotmail.co.uk>
2022-03-25 10:09:08 +0000

    form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,6 +4,19 @@ export default function Form() {
   return (
     <div className='form'>
       <h1 className='form__greeting'>Good evening, Olivia</h1>
+      <fieldset>
+        <label>From</label>
+        <input></input>
+        <label>To</label>
+        <input></input>
+        <div className='form__radio'>
+          radio
+        </div>
+        <div className='form__dropdown'>
+
+        </div>
+        <button className='form__submit'>Search</button>
+      </fieldset>
     </div>
   )
 }
