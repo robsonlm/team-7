efedf07cc26af5b27bee54c1bc9bdb91c9ca6b48
robsonlm <robsonlmaraia@gmail.com>
2022-03-25 09:57:25 +0000

    Matches component

--- src/App.js
+++ src/App.js
@@ -5,30 +5,34 @@ import FindMatch from "./components/matches/FindMatch";
 import Form from "./components/form/Form";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile/Profile";
+import { useEffect, useState } from "react";
+import data from "./data/data.json";
 
 function App() {
+  const [matchResults, setMatchResults] = useState(data);
+
   return (
     <>
-      <Header />
       <Switch>
-      <Route
-            path="/form"
-            exact
-            render={(renderProps) => <Form  {...renderProps} />}
-      />
-      <Route
-            path="/findmatch"
-            exact
-            render={(renderProps) => <FindMatch  {...renderProps} />}
-      />
-      <Route
-            path="/profile"
-            exact
-            render={(renderProps) => <Profile {...renderProps} />}
-      />
-      </ Switch>
-        
-        
+        <Route
+          path="/form"
+          exact
+          render={(renderProps) => <Form {...renderProps} />}
+        />
+        <Route
+          path="/matchresults"
+          exact
+          render={(renderProps) => (
+            <FindMatch matchResults={matchResults} {...renderProps} />
+          )}
+        />
+        <Route
+          path="/profile"
+          exact
+          render={(renderProps) => <Profile {...renderProps} />}
+        />
+      </Switch>
+
       <Footer />
     </>
   );
--- src/components/matches/FindMatch.js
+++ src/components/matches/FindMatch.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import backSVG from "../../assets/icons/back-icon.svg";
+import MatchesCard from "../MatchesCard/MatchesCard";
+import "./FindMatch.scss";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function FindMatch() {
+const FindMatch = ({ renderProps, matchResults }) => {
+  console.log(matchResults);
   return (
-    <div>FindMatch</div>
-  )
-}
+    <>
+      <section className="match-results">
+        <article className="match-results__wrapper">
+          {matchResults.length == 0 && (
+            <h1 className="match-results__title">No matches found</h1>
+          )}
+          {matchResults.length == 1 && (
+            <h1 className="match-results__title">1 match found</h1>
+          )}
+          {matchResults.length > 1 && (
+            <h1 className="match-results__title">
+              {matchResults.length} matches found
+            </h1>
+          )}
+          <Link to="/">
+            <img
+              className="match-results__back-icon"
+              src={backSVG}
+              alt="Return"
+            />
+          </Link>
+          <div className="match-results__cards">
+            {matchResults.map((match) => (
+              <MatchesCard key={match.id} match={match} {...renderProps} />
+            ))}
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default FindMatch;
