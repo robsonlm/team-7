2ce79deead9cec6787f9ca2ceeac42756254b195
Michael Webb <webbmichael@hotmail.co.uk>
2022-03-25 09:44:04 +0000

    added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Header } from "./components/Header/Header";
 import Footer from "./components/footer/Footer";
 import FindMatch from "./components/matches/FindMatch";
 import Form from "./components/form/Form";
+import './App.scss';
 import { Redirect, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <>
+ 
+    <div className="app">
       <Header />
       <Switch>
       <Route
@@ -22,7 +24,7 @@ function App() {
             render={(renderProps) => <FindMatch  {...renderProps} />}
       />
       <Route
-            path="/profile"
+            path="/profile/"
             exact
             render={(renderProps) => <Profile {...renderProps} />}
       />
@@ -30,7 +32,8 @@ function App() {
         
         
       <Footer />
-    </>
+   
+    </div>
   );
 }
 
