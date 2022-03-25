fc02242027b662c735a0f6070b035779c9ec3f3c
Michael Webb <webbmichael@hotmail.co.uk>
2022-03-25 15:33:07 +0000

    updated footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Footicon from '../footicon/Footicon'
 import './Footer.scss';
-import message from '../../assets/svg/message.svg';
-import profile from '../../assets/svg/profile.svg';
+import message from '../../assets/svg/chats.png.png';
+import profile from '../../assets/svg/profile.png.png';
 import search from '../../assets/svg/search.svg';
 export default function Footer() {
   return (
       <section className='footer'>
-    <Footicon icon = {message} label={'chats'} />
-    <Footicon icon = {search} label={'search'} link ={'/form'}/>
-    <Footicon icon = {profile} label={'profile'} link={'/profile/1'} />
+    <Footicon icon = {message} label={'chats'} white={''}/>
+    <div className='blueback'>
+      <Footicon icon = {search} label={'search'} link ={'/form'} white={'white'}/>
+    </div>
+    <Footicon icon = {profile} label={'profile'} link={'/profile/1'} white={''}/>
    
     </section>
   )
